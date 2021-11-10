af5364e3f2edce7c01dc4c1d8afcca972815fd6e
Scarnface <scarnface@tutanota.com>
2021-11-10 15:03:24 +0000

    Employees linked to companies through button on their entries.

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -9,6 +9,8 @@ class Employee extends Model
 {
     use HasFactory;
 
+    protected $guarded = [];
+
     public function company()
     {
         return $this->belongsTo(Company::class, 'company_id');
--- database/migrations/2021_11_05_093114_create_employees_table.php
+++ database/migrations/2021_11_05_093114_create_employees_table.php
@@ -17,7 +17,7 @@ class CreateEmployeesTable extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->foreignId('company_id');
+            $table->foreignId('company_id');//->constrained()->cascadeOnDelete();
             $table->string('company');
             $table->string('email')->unique();
             $table->char('phone_number', '22')->unique();
