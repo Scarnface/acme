2065aea3ecbde051cbc2a6cd4d23b09f70d142c9
Scarnface <scarnface@tutanota.com>
2021-11-15 14:14:51 +0000

    Fixed malformed flash messages due to template class overides.

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -2,8 +2,8 @@
   <div x-data="{ show: true }"
        x-init="setTimeout(() => show = false, 4000)"
        x-show="show"
-       class="fixed bg-red-800 text-white text-sm py-2 px-4 rounded-xl right-3 bottom-3"
+       class="fixed bg-red-800 text-white text-sm mb-0 py-2 px-4 rounded-xl right-3 bottom-3"
   >
-    <p>{{ session('success') }}</p>
+    <span>{{ session('success') }}</span>
   </div>
 @endif
