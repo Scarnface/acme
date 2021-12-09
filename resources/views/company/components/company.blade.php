b8d16615a6e90eb012322e1e07418e4c3713940a
Scarnface <scarnface@tutanota.com>
2021-12-09 15:01:47 +0000

    Feedback updates. Styling and cleanup.

--- resources/views/company/components/company.blade.php
+++ resources/views/company/components/company.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('company.components.clist')
+    <h1 class="text-3xl font-bold text-center mt-6 mb-6">COMPANY</h1>
+
+    <div class="flex justify-content-center mb-6">
+        @include('company.components.clist')
+    </div>
+
+    <a class="text-lg text-red-800 hover:text-gray-500" href="{{ route('home') }}">Return to dashboard</a>
 @endsection
--- resources/views/employee/components/employee.blade.php
+++ resources/views/employee/components/employee.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('employee.components.elist')
+    <h1 class="text-3xl font-bold text-center mt-6 mb-6">EMPLOYEE</h1>
+
+    <div class="mb-6">
+        @include('employee.components.elist')
+    </div>
+
+    <a class="text-lg text-red-800 hover:text-gray-500" href="{{ route('home') }}">Return to dashboard</a>
 @endsection
